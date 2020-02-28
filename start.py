6565cbad56445061e117b2efc5c276b46b2ed9b6
Juan Reto Reynal <juanreto@gmail.com>
2020-02-28 15:18:26 -0300

    Winter is coming

--- start.py
+++ start.py
@@ -1,12 +1,20 @@
 from PyQt5.QtWidgets import QApplication
 
-from webcam import Camera
-from mainGUI import StartWindow
+from models import Camera
+from Spectrometer import Spectrometer
+from views import StartWindow
+
 
 camera = Camera(0)
 camera.initialize()
 
+spectrometer = Spectrometer()
+spectrometer.connect()
+
 app = QApplication([])
-start_window = StartWindow(camera)
+start_window = StartWindow(camera,spectrometer)
+# start_window = StartWindow(camera)
 start_window.show()
 app.exit(app.exec_())
+spectrometer.disconnect()
+camera.close_camera()
---
+++ views.py
@@ -0,0 +1,154 @@
+from PyQt5.QtCore import Qt, QThread, QTimer
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import QMainWindow, QWidget, QPushButton, QGridLayout,QHBoxLayout, QApplication, QSlider
+from pyqtgraph import ImageView
+import pyqtgraph as pg
+import pyqtgraph.exporters
+import numpy as np
+import time
+import pandas as pd
+wavel_df = pd.read_pickle("wavel_df.pkl")
+wavel_array = wavel_df.iloc[:, 0].values
+
+
+class StartWindow(QMainWindow):
+    def __init__(self, camera = None,spectrometer=None):
+        super().__init__()
+        self.camera = camera
+        self.spectrometer = spectrometer
+
+        self.title = 'LECacqGUI'
+        self.left = 1000
+        self.top = 1000
+        self.width = 1000
+        self.height = 1000
+        self.icon = "logo_LEC.ico"
+
+        self.setWindowTitle(self.title)
+        self.setWindowIcon(QtGui.QIcon(self.icon))
+
+
+        self.central_widget = QWidget()
+        self.button_frame = QPushButton('Acquire Frame', self.central_widget)
+        self.button_frame.resize(5000,5000)
+        self.button_movie = QPushButton('Start Live View', self.central_widget)
+        # self.button_stop_movie = QPushButton('Stop Live View', self.central_widget)
+        self.button_single_spectra = QPushButton('Acquire Single Spectra', self.central_widget)
+        self.button_live_spectra = QPushButton('Start Live AcqSpectra', self.central_widget)
+        self.image_view = ImageView()
+        self.slider = QSlider(Qt.Horizontal)
+        self.slider.setRange(0,1000)
+        self.spectra_view = pg.GraphicsLayoutWidget()
+
+        self.layout = QGridLayout(self.central_widget)
+        self.showFullScreen()
+
+        self.layout.addWidget(self.image_view, 0, 0)
+        self.layout.addWidget(self.slider, 1, 0)
+        self.layout.addWidget(self.button_frame, 2, 0)
+        self.layout.addWidget(self.button_movie, 3, 0)
+        # self.layout.addWidget(self.button_stop_movie, 4, 0)
+        self.setCentralWidget(self.central_widget)
+
+
+        self.layout.addWidget(self.spectra_view,0,1)
+        self.layout.addWidget(self.button_single_spectra, 1, 1)
+        self.layout.addWidget(self.button_live_spectra, 2, 1)
+
+        self.spectra_plot = self.spectra_view.addPlot(title = 'Live Spectra Acquisition')
+        self.spectra_plot.showGrid(x=True,y=True,alpha=1.0)
+        x_axis = self.spectra_plot.getAxis('bottom')
+        y_axis = self.spectra_plot.getAxis('left')
+
+        x_axis.setLabel(text='Wavelength [nm]')  # set axis labels
+        y_axis.setLabel(text='Intensity [u.a.]')
+
+        self.drawplot = self.spectra_plot.plot(pen='y')
+
+
+        self.x = wavel_array
+        self.y = wavel_array
+        self.drawplot.setData(self.x, self.y)
+
+
+        QtGui.QApplication.processEvents()
+        self.exporter = pg.exporters.ImageExporter(self.spectra_view.scene())
+        self.image_counter = 1
+
+
+        self.button_frame.clicked.connect(self.update_image)
+        self.button_movie.clicked.connect(self.start_movie)
+        # self.button_stop_movie.clicked.connect(self.camera.stop)
+        self.button_single_spectra.clicked.connect(self.update_single_spectra)
+        self.button_live_spectra.clicked.connect(self.start_live_spectra)
+        self.slider.valueChanged.connect(self.update_brightness)
+
+        self.update_timer = QTimer()
+        self.update_timer.timeout.connect(self.update_movie)
+
+        self.spectra_update_timer = QTimer()
+        self.spectra_update_timer.timeout.connect(self.update_live_spectra)
+
+    def update_image(self):
+        self.camera.stop()
+        frame = self.camera.get_frame()
+        self.image_view.setImage(frame,autoHistogramRange=True)
+
+    def update_movie(self):
+        self.image_view.setImage(self.camera.last_frame)
+
+    def update_single_spectra(self):
+        self.spectrometer.stop()
+        single_spectra = self.spectrometer.measure_spectra(2,'50 ms')
+        self.drawplot.setData(single_spectra)
+
+    def update_live_spectra(self):
+        # intensity = self.spectrometer.measure_spectra(2,'50 ms')
+        self.drawplot.setData(self.spectrometer.last_intensity)
+        # self.update_framerate()
+
+    def update_brightness(self, value):
+        value /= 10
+        self.camera.set_brightness(value)
+
+    def start_movie(self):
+        self.camera.stopped = False
+        self.movie_thread = MovieThread(self.camera)
+        self.movie_thread.start()
+        self.update_timer.start(100)
+
+    def start_live_spectra(self):
+        self.spectrometer.stopped = False
+        self.spectra_thread = SpectraThread(self.spectrometer)
+        self.spectra_thread.start()
+        self.spectra_update_timer.start(10)
+
+
+class MovieThread(QThread):
+    def __init__(self, camera):
+        super().__init__()
+        self.camera = camera
+
+    def run(self):
+        while True:
+            if self.camera.stopped:
+                return
+            self.camera.acquire_movie(1)
+
+
+class SpectraThread(QThread):
+    def __init__(self, spectrometer):
+        super().__init__()
+        self.spectrometer = spectrometer
+
+    def run(self):
+        while True:
+            if self.spectrometer.stopped:
+                return
+            self.spectrometer.acquire_spectra(1,2,'50 ms')
+
+if __name__ == '__main__':
+    app = QApplication([])
+    window = StartWindow()
+    window.show()
+    app.exit(app.exec_())
